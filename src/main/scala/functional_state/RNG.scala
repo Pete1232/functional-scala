1acb407b936d18a1b0dbe0cfbb18c5ece5a4b2f5
Pete1232 <pete1232@gmail.com>
2016-09-11 09:17:34 +0100

    Added solution for sequence Also included the better solution from fpinscala for reference

--- src/main/scala/functional_state/RNG.scala
+++ src/main/scala/functional_state/RNG.scala
@@ -5,10 +5,10 @@ import functional_state.RNG.Rand
 trait RNG {
   def nextInt: (Int, RNG)
 
-  def map[A,B](s: Rand[A])(f: A => B): (B, RNG) =
+  def map[A, B](s: Rand[A])(f: A => B): (B, RNG) =
     RNG.map(s)(f)(this)
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): (C, RNG) =
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): (C, RNG) =
     RNG.map2(ra, rb)(f)(this)
 }
 
@@ -18,25 +18,41 @@ object RNG {
   type Rand[+A] = RNG => (A, RNG)
 
   implicit def randToResult[A](rand: (A, RNG)): A = rand._1
+
   implicit def randToRNG[A](rand: (A, RNG)): RNG = rand._2
 
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
     rng => {
       val (a, rng2) = s(rng)
       (f(a), rng2)
     }
-  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rng => {
       val (a, rng2) = ra(rng)
       val (b, rng3) = rb(rng2)
       (f(a, b), rng3)
     }
-  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    rng => {
+      val (list, r) =
+        fs.foldLeft(List[A](), rng) { (state, rand) =>
+          // these next three lines are very similar to map2; which hints that something isn't as good as it could be
+          // see comments after the function
+          // (didn't spot the repetition until I cross-checked against the solutions)
+          val (thisList, thisRng) = state
+          val (nextA, nextRng) = rand(thisRng)
+          (thisList :+ (nextA), nextRng)
+        }
+      (list, r)
+    }
+//  the neater fp in scala solution for reference
+//  fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
   def unit[A](a: A): Rand[A] = rng => (a, rng)
 
-  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
     map2(ra, rb)((_, _))
 
   case class SimpleRNG(seed: Long) extends RNG {
@@ -54,12 +70,12 @@ object RNG {
   //      (wasn't sure about how to keep this random - and this isn't perfect)
   //      iterate negative by 1 and change the sign
   def nonNegativeInt: Rand[Int] =
-    map(_.nextInt){ i =>
-      if(i < 0) -(i + 1) else i
+    map(_.nextInt) { i =>
+      if (i < 0) -(i + 1) else i
     }
 
   def double: Rand[Double] =
-    map(_.nextInt)(_.toDouble/Int.MaxValue)
+    map(_.nextInt)(_.toDouble / Int.MaxValue)
 
   def intDouble: Rand[(Int, Double)] =
     both(_.nextInt, double(_))
@@ -74,16 +90,6 @@ object RNG {
     ((first._1, second._1, third._1), third._2)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    def listBuilder(count: Int, thisList: List[Int] = List())(rng: RNG): (List[Int], RNG) = {
-      if(count > 0) {
-        listBuilder(count - 1, thisList :+ rng.nextInt._1)(rng.nextInt._2)
-      }
-      else {
-        //        If not using this rng it should be passed on to the next caller (so no nextInt here)
-        (thisList, rng)
-      }
-    }
-    listBuilder(count)(rng)
-  }
+  def ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(_.nextInt))
 }
